cb653e2cf000fe2e3644038c3a59a0c82e03dfc5
Enoc Delgado <105158271+EnocDelgado@users.noreply.github.com>
2023-05-18 22:02:36 -0500

    Section 11 finished

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -7,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18Select Pipe
+  public name: string = 'Enoc';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invite you',
+    'female': 'invite you'
+  }
+
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  // i18Plural Pipe
+  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Eduardo', 'John', 'Charles', 'Natalie'];
+  public clientMap = {
+    '=0': 'there are no customers waiting',
+    '=1': 'there is 1 customer waiting',
+    '=2': 'there are 2 customers waiting',
+    'other': 'there are # customers waiting'
+  }
+
+  deleteClient() {
+    this.clients.shift();
+  }
+
+  // keyValue Pipe
+  public person = {
+    name: 'Ethan',
+    age: 22,
+    address: 'Munich, Germany'
+  }
+
+  // Async Pipe
+  public myObservableTimer = interval( 2000 ).pipe(
+    tap( value => console.log('tap', value ) )
+  );
+
+  public promiseValue = new Promise( (resolve, reject ) => {
+    setTimeout( () => {
+      resolve( 'We have data on the proise')
+    }, 3500)
+  })
 }
